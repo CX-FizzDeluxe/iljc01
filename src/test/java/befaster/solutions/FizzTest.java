1065be8539a0ec8bad50130d4bae6b819341bbd8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-11 17:07:36 +0000

    Thu Jan 11 17:07:36 GMT 2018

--- src/test/java/befaster/solutions/FizzTest.java
+++ src/test/java/befaster/solutions/FizzTest.java
@@ -9,8 +9,7 @@ public class FizzTest {
 	
 	@Test
 	public void compute_fizz() {
-		//assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz fake deluxe"));
-		//assertThat(FizzBuzz.fizzBuzz(63), equalTo("fizz fake deluxe"));
+		
 		assertThat(FizzBuzz.fizzBuzz(35532), equalTo("fizz buzz deluxe"));
 		assertThat(FizzBuzz.fizzBuzz(225), equalTo("fizz buzz"));
 	}
