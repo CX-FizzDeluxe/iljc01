7a3ddc2018535a35e6d875d8dbc50b54f0598391
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-11 10:00:48 +0000

    Thu Jan 11 10:00:48 GMT 2018

--- src/test/java/befaster/solutions/FizzTest.java
+++ src/test/java/befaster/solutions/FizzTest.java
@@ -1,9 +1,15 @@
 package befaster.solutions;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class FizzTest {
 
 	public static void main(String[] args) {
-		System.out.println(FizzBuzz.fizzBuzz(2));
+		//System.out.println(FizzBuzz.fizzBuzz(2));
+		assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
 	}
 
 }
