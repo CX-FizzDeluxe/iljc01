c1f5f678f0f0325df73bdab3aa838f7b125ec5d6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-11 16:36:48 +0000

    Thu Jan 11 16:36:48 GMT 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,7 +5,7 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzz {
 	
     public static String fizzBuzz(Integer number) {
-    	boolean three = false, five = false, deluxe = false;
+    	boolean three = false, five = false, deluxe = false, fake = false;
     	int numCopy = number, tmp;
     	
     	if(number % 3 == 0) {
@@ -49,17 +49,30 @@ public class FizzBuzz {
     	if(numCopy == 0 && number > 10) {
     		deluxe = true;
     	}
+    	
+    	if(number % 2 != 0) {
+    		fake = true;
+    		deluxe = false;
+    	}
     		
     	if(three == true && five == true && deluxe == true) {
     		return "fizz buzz deluxe";
+    	} else if(three == true && five == true && fake == true) {
+    		return "fizz buzz fake deluxe";
     	} else if(three == true && five == true) {
     		return "fizz buzz";
     	} else if(three == true && deluxe == true) {
     		return "fizz deluxe";
     	} else if(five == true && deluxe == true) {
     		return "buzz deluxe";
+    	} else if(three == true && fake == true) {
+    		return "fizz fake deluxe";
+    	} else if(five == true && fake == true) {
+    		return "buzz fake deluxe";
     	} else if(deluxe == true) {
     		return "deluxe";
+    	} else if(fake == true) {
+    		return "fake deluxe";
     	} else if(three == true) {
     		return "fizz";
     	} else if(five == true) {
--- src/test/java/befaster/solutions/FizzTest.java
+++ src/test/java/befaster/solutions/FizzTest.java
@@ -9,10 +9,10 @@ public class FizzTest {
 	
 	@Test
 	public void compute_fizz() {
-		assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz deluxe"));
+		assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz fake deluxe"));
 		assertThat(FizzBuzz.fizzBuzz(250), equalTo("buzz"));
-		assertThat(FizzBuzz.fizzBuzz(36), equalTo("fizz"));
-		assertThat(FizzBuzz.fizzBuzz(225), equalTo("fizz buzz"));
+		assertThat(FizzBuzz.fizzBuzz(37), equalTo("fizz fake deluxe"));
+		assertThat(FizzBuzz.fizzBuzz(225), equalTo("fizz buzz fake deluxe"));
 	}
 
 }
