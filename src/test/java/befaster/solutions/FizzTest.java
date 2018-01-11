6bbae3373c2d4d45f0833ac7eef1ce7f7c241f87
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-11 09:57:48 +0000

    Thu Jan 11 09:57:48 GMT 2018

--- src/test/java/befaster/solutions/FizzTest.java
+++ src/test/java/befaster/solutions/FizzTest.java
@@ -3,8 +3,7 @@ package befaster.solutions;
 public class FizzTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(FizzBuzz.fizzBuzz(2));
 	}
 
 }
