050a6eff9512dd95ea70a8bf976868d4ffcd7e8a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-11 09:54:48 +0000

    Thu Jan 11 09:54:48 GMT 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,7 +5,34 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+    	boolean threeDiv = false, fiveDiv = false;
+    	int numCopy = number;
+    	
+    	while(numCopy/3 == (int) numCopy/3) {
+    		if(numCopy == 1) return "fizz";
+    	}
+    	
+    	numCopy = number;
+    	while(numCopy/5 == (int) numCopy/5) {
+    		if(numCopy == 1) return "buzz";
+    	}
+    	
+    	numCopy = number;
+    	while(numCopy != 1) {
+    		if(numCopy % 3 == 0) {
+    			threeDiv = true;
+    			numCopy /= 3;
+    		}
+    		
+    		if(numCopy % 5 == 0) {
+    			fiveDiv = true;
+    			numCopy /= 5;
+    		}
+    		
+    		if(numCopy == 1 && threeDiv == true && fiveDiv == true) return "fizz buzz";
+    	}
+    	
+    	return Integer.toString(number);
     }
 
 }
