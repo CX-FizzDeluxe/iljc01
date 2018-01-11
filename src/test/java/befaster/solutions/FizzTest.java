fc8cff2859951acf3229be934f38224f614686cb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-11 10:03:48 +0000

    Thu Jan 11 10:03:48 GMT 2018

--- src/test/java/befaster/solutions/FizzTest.java
+++ src/test/java/befaster/solutions/FizzTest.java
@@ -6,10 +6,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class FizzTest {
-
-	public static void main(String[] args) {
-		//System.out.println(FizzBuzz.fizzBuzz(2));
+	
+	@Test
+	public void compute_fizz() {
 		assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
+		assertThat(FizzBuzz.fizzBuzz(5), equalTo("buzz"));
+		assertThat(FizzBuzz.fizzBuzz(9), equalTo("fizz"));
+		assertThat(FizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
 	}
 
 }
